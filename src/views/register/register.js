59c8d532bc829f30adc024a9db323da17302c5a9
wanzz8 <white.wanj@gmail.com>
2018-12-05 15:35:02 +0800

    1. 注册头部不兼容的问题

--- src/views/register/register.js
+++ src/views/register/register.js
@@ -11,7 +11,7 @@ import {
     ImageBackground,
     Image,
     TextInput,
-    Keyboard
+    Keyboard, Platform
 } from 'react-native';
 import { observer, inject } from 'mobx-react/native';
 import Icons from 'react-native-vector-icons/Ionicons';
@@ -24,6 +24,20 @@ import {
 const width = Dimensions.get('window').width; // 全屏宽高
 const height = Dimensions.get('window').height; // 全屏宽高
 const IMG = require('../../img/mine/lock.png');
+
+const X_WIDTH = 375;
+const X_HEIGHT = 812;
+
+function isIphoneX() {
+    return (
+        (Platform.OS === 'ios'
+            && ((height === X_HEIGHT
+                && width === X_WIDTH)
+                || (height === X_WIDTH
+                    && width === X_HEIGHT)))
+        || Platform.OS === 'android'
+    );
+}
 
 @inject('CacheStore')
 class Register extends Component {
@@ -41,10 +55,30 @@ class Register extends Component {
             >
                 <Icons name="ios-arrow-back" size={25} color="#FFF" />
             </TouchableOpacity>),
+        headerRight: (
+            <View>
+                <Text style={{
+                    fontSize: 18,
+                    color: '#fff',
+                    fontWeight: 'bold',
+                    marginRight: 10
+                }}
+                />
+            </View>),
         headerTitleStyle: {
-            alignSelf: 'center', fontSize: 18, color: '#fff', fontWeight: 'bold'
+            alignSelf: 'center',
+            flex: 1,
+            textAlign: 'center',
+            fontSize: 18,
+            color: '#fff',
+            fontWeight: 'bold'
         },
-        headerStyle: { height: 35, backgroundColor: '#292929' }
+        headerStyle: {
+            height: isIphoneX() ? 65 : 45,
+            backgroundColor: '#292929',
+            paddingTop: isIphoneX() ? 20 : 0,
+            elevation: 0,
+        }
     });
 
     constructor(props) {
